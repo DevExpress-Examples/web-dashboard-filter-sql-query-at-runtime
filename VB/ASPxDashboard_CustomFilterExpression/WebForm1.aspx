594ff4f891c9528d9b37e0a01acdd37869f13aeb
DevExpressExampleBot <fakeEmail>
2022-10-18 16:58:58 +0400

    Source auto update [skip ci]

--- VB/ASPxDashboard_CustomFilterExpression/WebForm1.aspx
+++ VB/ASPxDashboard_CustomFilterExpression/WebForm1.aspx
@@ -1,18 +1,18 @@
-﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="WebForm1.aspx.vb" Inherits="ASPxDashboard_CustomFilterExpression.WebForm1" %>
+<%@ Page Language="VB" AutoEventWireup="true" CodeBehind="WebForm1.aspx.vb" Inherits="ASPxDashboard_CustomFilterExpression.WebForm1" %>
 <!DOCTYPE html>
 
 <html>
 <head runat="server">
-	<title></title>
+    <title></title>
 </head>
 <body>
-	<form id="form1" runat="server">
-	<div style="position: absolute; top: 0; left: 0; right: 0; bottom: 0;">
-		<dx:ASPxDashboard ID="ASPxDashboard1" runat="server" Width="100%" Height="100%" 
-			OnCustomFilterExpression="ASPxDashboard1_CustomFilterExpression" 
-			WorkingMode="Viewer">
-		</dx:ASPxDashboard>
-	</div>
-	</form>
+    <form id="form1" runat="server">
+    <div style="position: absolute; top: 0; left: 0; right: 0; bottom: 0;">
+        <dx:ASPxDashboard ID="ASPxDashboard1" runat="server" Width="100%" Height="100%" 
+            OnCustomFilterExpression="ASPxDashboard1_CustomFilterExpression" 
+            WorkingMode="Viewer">
+        </dx:ASPxDashboard>
+    </div>
+    </form>
 </body>
 </html>
--- VB/ASPxDashboard_CustomFilterExpression/WebForm1.aspx.vb
+++ VB/ASPxDashboard_CustomFilterExpression/WebForm1.aspx.vb
@@ -1,19 +1,20 @@
-﻿Imports System
+Imports System
 Imports DevExpress.DashboardWeb
 Imports DevExpress.Data.Filtering
 
 Namespace ASPxDashboard_CustomFilterExpression
-	Partial Public Class WebForm1
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-			ASPxDashboard1.SetDashboardStorage(New DashboardFileStorage("~/App_Data/Dashboards"))
-		End Sub
+    Public Partial Class WebForm1
+        Inherits Web.UI.Page
 
-		Protected Sub ASPxDashboard1_CustomFilterExpression(ByVal sender As Object, ByVal e As CustomFilterExpressionWebEventArgs)
-			If e.DataSourceConnectionName = "nwindConnection" AndAlso e.TableName = "Invoices" Then
-				e.FilterExpression = New BinaryOperator("CustomerID", "AROUT", BinaryOperatorType.Equal)
-			End If
-		End Sub
-	End Class
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ASPxDashboard1.SetDashboardStorage(New DashboardFileStorage("~/App_Data/Dashboards"))
+        End Sub
+
+        Protected Sub ASPxDashboard1_CustomFilterExpression(ByVal sender As Object, ByVal e As CustomFilterExpressionWebEventArgs)
+            If Equals(e.DataSourceConnectionName, "nwindConnection") AndAlso Equals(e.QueryName, "Invoices") Then
+                e.FilterExpression = New BinaryOperator("Customers.CompanyName", "Around the Horn", BinaryOperatorType.Equal)
+            End If
+        End Sub
+    End Class
 End Namespace
